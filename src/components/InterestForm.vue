0d1a2cd01995fee0448952ba3acd136caa6d6a32
maxarvid <max.arvid.anderson@gmail.com>
2022-09-12 11:44:44 +0200

    WIP adds an input form

---
+++ src/components/InterestForm.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const firstName = ref('')
+const handleSubmit = (event) => {
+  debugger;
+};
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input
+      v-model="firstName"
+      type="text"
+      placeholder="Your Name"
+    />
+    <p> Input is {{ firstName }}</p>
+    <input type="text" placeholder="Your favourite animal" />
+    <button>Submit</button>
+  </form>
+</template>
+
+<style scoped></style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,9 @@
+<script setup>
+import InterestForm from "../components/InterestForm.vue";
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <InterestForm />
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
